7b05b94340996c3250c1127057b0c269855f7ada
Roman <Nemchinovrp@yandex.ru>
2013-12-23 06:16:14 +0800

    Create Table.java

---
+++ Table.java
@@ -0,0 +1,28 @@
+
+public class Table{
+      static void compute(){
+      int k = 0;
+  
+      for(int i=1; i < 11; i++){
+      
+      for(int j=1; j<11; j++){
+      
+      k = i * j;
+      
+      System.out.print(k + " " );
+      
+      }
+      
+      System.out.println();
+      
+      }
+      
+      }
+      
+      public static void main(String[] args){
+      
+          compute();
+      
+      }
+
+}
